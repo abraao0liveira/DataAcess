ea30c36c84c15c8440cf72c7ced421d209feeee8
abraao0liveira <abraaooliveira.br@gmail.com>
2025-03-30 04:02:07 -0300

    Initial commit

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace DataAcess.Models;
+
+public class Category
+{ 
+	public Guid Id { get; set; }
+	public required string Title { get; set; }
+	public required string Url { get; set; }
+	public required string Summary { get; set; }
+	public int Order { get; set; }
+	public required string Description { get; set; }
+	public bool Featured { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using Dapper;
+using DataAcess.Models;
+using Microsoft.Data.SqlClient;
+
+namespace DataAcess;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const string connectionString =
+            "Server=localhost;Database=study_db;User Id=sa;Password=C3rul3@nC@v3_150;Trusted_Connection=False;MultipleActiveResultSets=true;TrustServerCertificate=true;";
+
+        using (var connection = new SqlConnection(connectionString))
+        {
+            //UpdateCategory(connection);
+            // CreateCategory(connection);
+            ListCategories(connection);
+        }
+    }
+
+    static void ListCategories(SqlConnection connection)
+    {
+        const string sqlSelect = " select [id], [title] from [category] ";
+        var categories = connection.Query<Category>(sqlSelect);
+
+        foreach (var item in categories)
+        {
+            Console.WriteLine($"{item.Id} - {item.Title}");
+        }
+    }
+
+    static void CreateCategory(SqlConnection connection)
+    {
+        var category = new Category
+        {
+            Id = Guid.NewGuid(),
+            Title = "Amazon AWS",
+            Url = "https://aws.amazon.com",
+            Summary = "AWS Cloud",
+            Order = 8,
+            Description = "Categoria de cursos de aws",
+            Featured = true
+        };
+
+        const string sqlInsert = " insert into [category] " +
+                                 " values(@Id, @Title, @Url, @Summary, @Order, @Description, @Featured) ";
+
+        var rows = connection.Execute(sqlInsert, new
+        {
+            Id = category.Id,
+            Title = category.Title,
+            Url = category.Url,
+            Summary = category.Summary,
+            Order = category.Order,
+            Description = category.Description,
+            Featured = category.Featured
+        });
+        Console.WriteLine($"Inserted {rows} rows");
+    }
+
+    static void UpdateCategory(SqlConnection connection)
+    {
+        const string sqlUpdate = " update [category] set [title] = @Title where [id] = @Id";
+        var rows = connection.Execute(sqlUpdate, new
+        {
+            Id = new Guid("100708e9-6b66-4f2d-8c24-af447dbcf1b2"),
+            Title = "Frontend"
+        });
+        Console.WriteLine($"Updated {rows} rows");
+    }
+}
